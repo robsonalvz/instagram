ca036b9fa66e410b4dc9293187d16b07edee63d1
robsonalvz <robson.alves@dce.ufpb.br>
2019-06-13 14:02:02 -0300

    resize images to 500px

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 
 mongoose.connect('mongodb+srv://sisalfa:sisalfa@cluster0-oq7yn.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser:true,
 })
+
+app.use('/files', express.static(path.resolve(__dirname,'..','..','uploads','resized')));
 
 app.use(require('./routes.js'))
 
