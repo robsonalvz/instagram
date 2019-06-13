cd585e6d027833499da3eea02e924a636d2d5f2c
robsonalvz <robson.alves@dce.ufpb.br>
2019-06-13 13:06:40 -0300

    likes function added

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -2,9 +2,20 @@ const Post = require('../models/Post')
 
 module.exports = {
   async index(req,res) {
-
+      const posts = await Post.find().sort('-createdAt');
+      res.json(posts);
   },
   async store(req,res){
-    return res.json({ ok:true})
+    console.log(req.body);
+    const { author, place, description, hashtags} = req.body;
+    const { filename: image} = req.file;
+    const post = await Post.create({
+      author,
+      place,
+      description,
+      hashtags,
+      image,
+    });
+    return res.json(post);
    }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,13 @@
+const multer = require('multer')
 const PostController = require('./controllers/PostController');
+const LikeController = require('./controllers/LikeController');
+const uploadConfig = require('./config/upload')
 const express = require('express')
+
 const routes = express.Router();
 
-
-routes.post('/posts', PostController.store);
-module.exports = routes;
+const upload = multer(uploadConfig);
+routes.get('/posts',PostController.index);
+routes.post('/posts', upload.single('image'), PostController.store);
+routes.post('/posts/:id/like', LikeController.store);
+module.exports = routes; 
