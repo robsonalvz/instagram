c8371ee62b6669da02c0517611ed41f3c439e3ee
robsonalvz <robson.alves@dce.ufpb.br>
2019-06-13 10:32:02 -0300

    PostController added on index

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,5 +1,10 @@
 const Post = require('../models/Post')
 
 module.exports = {
-  async index(req,res)  
+  async index(req,res) {
+
+  },
+  async store(req,res){
+    return res.json({ ok:true})
+  }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,7 @@
+const PostController = require('./controllers/PostController');
 const express = require('express')
-
 const routes = express.Router();
 
 
-routes.get('/', (req,res) => {
-    return res.send('helo woe');
-});
+routes.post('/posts', PostController.store);
 module.exports = routes;
